d5aa30fa9705d7a3021e9f099a529303d06d03bf
noaa <noaamaman325158@gmail.com>
2023-10-30 02:34:07 +0200

    finale changes for the blueprint for the FastAPI that we build for creating some

--- routes/route.py
+++ routes/route.py
@@ -21,11 +21,30 @@ async def post_todo(todo: Todo):
     collection_name.insert_one(dict(todo))
 
 
+# Get Request method
 @router.put("/{id}")
 async def put_todo(id: str, todo: Todo):
     collection_name.find_one_and_update({"_id": ObjectId(id)}, {"$set": dict(todo)})
 
 
+# Delete Request method
 @router.delete("/{id}")
 async def delete_todo(id: str):
     collection_name.find_one_and_delete({"_id": ObjectId(id)})
+
+
+# PATCH Request Method
+@router.patch("/{id}")
+async def patch_todo(id: str, updated_fields: dict):
+    todo = collection_name.find_one({"_id": ObjectId(id)})
+    if not todo:
+        return {"message": "Todo not found"}
+
+    # Update the specified fields in the Todo
+    for key, value in updated_fields.items():
+        todo[key] = value
+
+    # Save the updated Todo
+    collection_name.update_one({"_id": ObjectId(id)}, {"$set": todo})
+
+    return todo
